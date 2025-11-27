cfcec9ec11563216135af4f764f3f5f5a988aaed
Santiago Cabral <santiagonahuelcabral10@gmail.com>
2025-11-27 10:57:39 -0300

    ya esta perrito

--- DTOs/SaleItemDto.cs
+++ DTOs/SaleItemDto.cs
@@ -4,7 +4,7 @@ namespace ForrajeriaJovitaAPI.DTOs
     {
         public int ProductId { get; set; }
         public string ProductName { get; set; } = "";
-        public int Quantity { get; set; }
+        public decimal Quantity { get; set; }
         public decimal UnitPrice { get; set; }
         public decimal Discount { get; set; }
         public decimal Total { get; set; }
