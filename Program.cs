a3c3ac6fd2a0e77517289ce2ca52528fdf88914f
Santiago Cabral <santiagonahuelcabral10@gmail.com>
2025-11-26 23:22:08 -0300

    correccion

--- Program.cs
+++ Program.cs
@@ -1,88 +1,51 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
-using ForrajeriaJovitaAPI.Data;
+﻿using ForrajeriaJovitaAPI.Data;
 using ForrajeriaJovitaAPI.Services;
 using ForrajeriaJovitaAPI.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Puerto para Render
-var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
-
 // DB
 builder.Services.AddDbContext<ForrajeriaContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Servicios
-builder.Services.AddScoped<IProductoService, ProductoService>();
-builder.Services.AddScoped<IStockService, StockService>();
-builder.Services.AddScoped<IBranchService, BranchService>();
+// Services
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IProductoService, ProductoService>();
+builder.Services.AddScoped<IVentaService, VentaService>();
+builder.Services.AddScoped<IStockService, StockService>();
+builder.Services.AddScoped<IClientService, ClientService>();
+builder.Services.AddScoped<IBranchService, BranchService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ICheckoutService, CheckoutService>();
+builder.Services.AddScoped<IClientAccountService, ClientAccountService>();
 
 // JWT
-var jwtKey = Environment.GetEnvironmentVariable("JWT_KEY");
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+var key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]);
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
 .AddJwtBearer(options =>
 {
     options.TokenValidationParameters = new TokenValidationParameters
     {
-        ValidateIssuerSigningKey = true,
         ValidateIssuer = false,
         ValidateAudience = false,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key)
     };
 });
 
-// 🔥 Todo es público salvo [Authorize]
-builder.Services.AddAuthorization(options =>
-{
-    options.FallbackPolicy = new Microsoft.AspNetCore.Authorization.AuthorizationPolicyBuilder()
-        .RequireAssertion(_ => true)
-        .Build();
-});
-
-// CORS
-builder.Services.AddCors(p => p.AddPolicy("AllowAll", b =>
-{
-    b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
-}));
-
-// Controllers
 builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = "Bearer {token}",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme { Reference = new OpenApiReference
-                { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
-            },
-            Array.Empty<string>()
-        }
-    });
-});
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
-
-app.UseCors("AllowAll");
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
--- Services/ProductoService.cs
+++ Services/ProductoService.cs
@@ -69,6 +69,7 @@ namespace ForrajeriaJovitaAPI.Services
 
                 // STOCK DECIMAL ✔
                 Stock = product.ProductsStocks.Sum(s => s.Quantity)
+
             };
         }
 
