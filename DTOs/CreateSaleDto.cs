10dd0d2101206029f86887ef3abf33bea9ac6d9b
Santiago Cabral <santiagonahuelcabral10@gmail.com>
2026-02-07 12:04:54 -0300

    correccion ventaservice y sale

--- DTOs/CreateSaleDto.cs
+++ DTOs/CreateSaleDto.cs
@@ -11,18 +11,5 @@ namespace ForrajeriaJovitaAPI.DTOs
         public List<CreateSalePaymentDto>? Payments { get; set; }
     }
 
-    public class CreateSaleItemDto
-    {
-        public int ProductId { get; set; }
-        public int Quantity { get; set; }
-        public decimal UnitPrice { get; set; }
-        public decimal Discount { get; set; }
-    }
-
-    public class CreateSalePaymentDto
-    {
-        public string Method { get; set; } = string.Empty;
-        public decimal Amount { get; set; }
-        public string? Reference { get; set; }
-    }
+    
 }
